e64ae1b546b6b85c6f549667f2e6bc6abe4860d8
Andrii Tarykin <ksbomj@gmail.com>
2022-07-01 21:58:34 +0300

    fix: update client helper when saving device without extra confirmation

--- src/app/Providers/EventServiceProvider.php
+++ src/app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\DeviceSaved;
+use App\Listeners\ClientHelperSave;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -19,6 +21,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
             RegisterOBADAAccount::class
+        ],
+        DeviceSaved::class => [
+            ClientHelperSave::class
         ],
     ];
 
