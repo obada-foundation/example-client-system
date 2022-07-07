7810e4b2dfe68ca6eded34c7eb4d70c5cfc268e1
Kate Tymoshkina <e.teamosh@gmail.com>
2022-07-07 10:20:07 +0300

    fixed wrapping for date

--- src/resources/js/modules/devices/list/index.js
+++ src/resources/js/modules/devices/list/index.js
@@ -99,7 +99,7 @@ $(document).ready(() => {
                 sortable: true,
                 "render": {
                     _: function (data, type, full, meta) {
-                        return '<span data-bs-toggle="tooltip" title="' + moment(full.created_at).format('MMM D YYYY, LT') + '">' + moment(full.created_at).fromNow() + '</span>';
+                        return '<span data-bs-toggle="tooltip" title="' + moment(full.created_at).format('MMM D YYYY, LT') + '" style="white-space: nowrap">' + moment(full.created_at).fromNow() + '</span>';
                     },
                     sort: function(data, type, full, meta) {
                         return full.created_at
