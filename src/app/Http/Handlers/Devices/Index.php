1a060c75fe8bdb0bc74e42e7e1e17037ef93c41e
Kate Tymoshkina <e.teamosh@gmail.com>
2022-09-23 14:04:50 +0000

    260 home page updates

--- src/app/Http/Handlers/Devices/Index.php
+++ src/app/Http/Handlers/Devices/Index.php
@@ -1,14 +1,33 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
 namespace App\Http\Handlers\Devices;
 
+use App\ClientHelper\Token;
 use App\Http\Handlers\Handler;
+use Illuminate\Support\Facades\Auth;
+use Obada\Api\AccountsApi;
 
 class Index extends Handler {
     public function __invoke()
     {
-        return view('devices.index');
+        $user = Auth::user();
+        $token = app(Token::class)->create($user);
+
+        $api = app(AccountsApi::class);
+        $api->getConfig()
+            ->setAccessToken($token);
+
+        $balance = $api->balance();
+
+
+        $devices_count = $user->devices()->count();
+
+        return view('devices.index', [
+            'address' => $balance->getAddress(),
+            'balance' => $balance->getBalance(),
+            'devices_count' => $devices_count
+        ]);
     }
 }
