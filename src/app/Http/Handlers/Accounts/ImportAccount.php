e72afba8852fb52bc78d319ab801ae2649b13549
Andrii Tarykin <techops@obada.io>
2023-01-26 21:38:11 +0000

    updated API to set account name

--- src/app/Http/Handlers/Accounts/ImportAccount.php
+++ src/app/Http/Handlers/Accounts/ImportAccount.php
@@ -7,19 +7,10 @@ namespace App\Http\Handlers\Accounts;
 use App\Http\Handlers\Handler;
 use Illuminate\Http\Request;
 use function view;
-use Illuminate\Support\Facades\Auth;
-use App\ClientHelper\Token;
-use Obada\Api\AccountsApi;
 
 class ImportAccount extends Handler {
     public function __invoke(Request $request)
     {
-        $token = app(Token::class)->create(Auth::user());
-
-        $api = app(AccountsApi::class);
-        $api->getConfig()
-            ->setAccessToken($token);
-
         return view('accounts.import-account');
     }
 }
--- src/app/Http/Handlers/Accounts/StoreAccount.php
+++ src/app/Http/Handlers/Accounts/StoreAccount.php
@@ -7,10 +7,8 @@ namespace App\Http\Handlers\Accounts;
 use App\Http\Handlers\Handler;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
-use Exception;
-use Illuminate\Support\Facades\Validator;
 use Obada\Api\AccountsApi;
-use Obada\ClientHelper\MnemonicRequest;
+use Obada\ClientHelper\AccountRequest;
 use App\ClientHelper\Token;
 use Illuminate\Support\Facades\Auth;
 use Obada\ApiException;
@@ -25,7 +23,7 @@ class StoreAccount extends Handler {
             ->setAccessToken($token);
 
         try {
-            $api->newAccount();
+            $api->newAccount(new AccountRequest);
         } catch (ApiException $e) {
             $newAccountError = json_decode($e->getResponseBody());
 
