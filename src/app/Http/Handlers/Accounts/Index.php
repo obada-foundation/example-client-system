35af0976203e5753345baa37f3059edf96a2e879
Andrii Tarykin <techops@obada.io>
2023-01-28 02:00:24 +0000

    seed phrease show

--- src/app/Http/Handlers/Accounts/Index.php
+++ src/app/Http/Handlers/Accounts/Index.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use App\ClientHelper\Token;
 use Obada\Api\AccountsApi;
 use Obada\Api\KeysApi;
+use Obada\ApiException;
 
 class Index extends Handler {
     public function __invoke(Request $request)
@@ -23,17 +24,29 @@ class Index extends Handler {
 
         $accounts = $api->accounts();
 
+        $mnemonic = '';
+
+        try {
+            $mnemonic = $api->getMnemonic();
+        } catch (ApiException $e) {
+            $error = json_decode($e->getResponseBody())->error;
+
+            if (strpos($error, "profile wallet doesn't exists") === false) {
+                report($e);
+            }
+        }
+
         $proccessAccounts = function (array $accounts) {
             return collect($accounts)
                 ->map(function ($account) {
-
                     $address = $account->getAddress();
+                    $name    = $account->getName()
+                        ? $account->getName() . ' (' . substr($address, -4) . ')'
+                        : substr($address, 0, 10) . '...' . substr($address, -4);
 
                     return [
                         'address'       => $address,
-                        'name'          => $account->getName()
-                            ? $account->getName() . ' (' . substr($address, -4) . ')'
-                            : substr($address, 0, 10) . '...' . substr($address, -4),
+                        'name'          => $name,
                         'balance'       => $account->getBalance(),
                         'pub_key'       => $account->getPubKey(),
                         'nft_count'     => $account->getNftCount(),
@@ -42,9 +55,15 @@ class Index extends Handler {
                 ->toArray();
         };
 
+       $words = explode(" ", $mnemonic->getMnemonic());
+
+       $shortMnemonic = count($words)
+            ? sprintf("%s .. %s", $words[0], last($words))
+            : "";
+
         return view('accounts.index', [
-            'seed_phrase' => '1 2 3 4 5 6 7 8 9',
-            'seed_phrase_short' => '1 ... 2',
+            'seed_phrase'       => $mnemonic->getMnemonic(),
+            'seed_phrase_short' => $shortMnemonic,
             'add_new_address'   => $request->has('add_new_address'),
             'hd_accounts'       => $proccessAccounts($accounts->getHdAccounts()),
             'imported_accounts' => $proccessAccounts($accounts->getImportedAccounts()),
