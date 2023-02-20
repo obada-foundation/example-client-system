4ebad5fde4f7b4a821744346e853b25ae471a448
Kate Tymoshkina <e.teamosh@gmail.com>
2023-02-20 14:26:33 +0100

    added setting to adjust logo height (#40)

--- src/config/frontend.php
+++ src/config/frontend.php
@@ -3,5 +3,6 @@
 return [
     'logo_path'   => env('FRONTEND_LOGO_PATH', '/images/obada-logo.svg'),
     'logo_text'   => env('FRONTEND_LOGO_TEXT', 'Blockchain Demo Site'),
+    'logo_height' => env('FRONTEND_LOGO_HEIGHT', '20'),
     'footer_text' => env('FRONTEND_FOOTER_TEXT', 'Copyright © 2018-2023 OBADA Foundation'),
 ];
